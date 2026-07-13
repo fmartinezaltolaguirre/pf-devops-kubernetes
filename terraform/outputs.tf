1140d13a525cc7fdcd2d1841ff0b0e3003fefe36
Fernando Martínez <96567345+fmartinezaltolaguirre@users.noreply.github.com>
2026-07-13 14:18:45 +0200

    Define outputs for networking and security modules
    Added multiple outputs for VPC, ECR, EKS, KMS, and Route 53.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,28 @@
-output "project_name" {
-  value = "techwave-devops"
+#############################################################
+# Root Outputs
+#############################################################
+
+output "vpc_id" {
+  value = module.networking.vpc_id
 }
+
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "kms_key_arn" {
+  value = module.security.kms_key_arn
+}
+
+output "hosted_zone_id" {
+  value = module.route53.hosted_zone_id
+}
+``
