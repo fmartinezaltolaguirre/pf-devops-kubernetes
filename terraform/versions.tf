ecf72f411ff51cf24cfa01978e21ce9f9bfeab53
Fernando Martínez <96567345+fmartinezaltolaguirre@users.noreply.github.com>
2026-07-13 14:17:24 +0200

    Add Terraform version and provider requirements

---
+++ terraform/versions.tf
@@ -0,0 +1,18 @@
+#############################################################
+# Terraform and Provider Versions
+#############################################################
+
+terraform {
+
+  required_version = ">= 1.8"
+
+  required_providers {
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+  }
+
+}
