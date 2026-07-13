d24201fc00a63c9041e735bd0b99c2d538af93d2
Fernando Martínez <96567345+fmartinezaltolaguirre@users.noreply.github.com>
2026-07-13 14:24:16 +0200

    Add DNS management comments in main.tf
    Add comments for DNS management in Route53 module

---
+++ terraform/modules/route53/main.tf
@@ -0,0 +1,7 @@
+#############################################################
+# DNS Management
+#
+# app.techwave.com
+# grafana.techwave.com
+# prometheus.techwave.com
+#############################################################
