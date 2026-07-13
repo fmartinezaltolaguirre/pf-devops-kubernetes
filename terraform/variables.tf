bd40cf28fc1d38eb029ed64c130639033125131e
Fernando Martínez <96567345+fmartinezaltolaguirre@users.noreply.github.com>
2026-07-13 14:18:23 +0200

    Add global variables for project configuration
    Added global variables for project name, AWS region, and VPC CIDR.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,45 @@
-variable "resource_group_name" {
+#############################################################
+# Global Variables
+#############################################################
+
+variable "project_name" {
+  type    = string
+  default = "techwave"
+}
+
+variable "environment" {
   type = string
 }
 
-variable "location" {
+variable "aws_region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnet_a_cidr" {
+  type = string
+}
+
+variable "public_subnet_b_cidr" {
+  type = string
+}
+
+variable "private_subnet_a_cidr" {
+  type = string
+}
+
+variable "private_subnet_b_cidr" {
+  type = string
+}
+
+variable "availability_zone_a" {
+  type = string
+}
+
+variable "availability_zone_b" {
   type = string
 }
