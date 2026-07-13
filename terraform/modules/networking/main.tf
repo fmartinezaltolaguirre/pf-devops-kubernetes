c3fddb74ce06bc86bb429745efc2bf1414c94614
Fernando Martínez <96567345+fmartinezaltolaguirre@users.noreply.github.com>
2026-07-13 14:20:16 +0200

    Add networking module for VPC and subnets
    This module creates a VPC along with public and private subnets for the specified environment.

---
+++ terraform/modules/networking/main.tf
@@ -0,0 +1,54 @@
+#############################################################
+# Networking Module
+#
+# Creates:
+# - VPC
+# - Public Subnets
+# - Private Subnets
+#############################################################
+
+resource "aws_vpc" "techwave" {
+
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "vpc-techwave-${var.environment}"
+  }
+
+}
+
+resource "aws_subnet" "public_a" {
+
+  vpc_id            = aws_vpc.techwave.id
+  cidr_block        = var.public_subnet_a_cidr
+  availability_zone = var.availability_zone_a
+
+  map_public_ip_on_launch = true
+
+}
+
+resource "aws_subnet" "public_b" {
+
+  vpc_id            = aws_vpc.techwave.id
+  cidr_block        = var.public_subnet_b_cidr
+  availability_zone = var.availability_zone_b
+
+  map_public_ip_on_launch = true
+
+}
+
+resource "aws_subnet" "private_a" {
+
+  vpc_id            = aws_vpc.techwave.id
+  cidr_block        = var.private_subnet_a_cidr
+  availability_zone = var.availability_zone_a
+
+}
+
+resource "aws_subnet" "private_b" {
+
+  vpc_id            = aws_vpc.techwave.id
+  cidr_block        = var.private_subnet_b_cidr
+  availability_zone = var.availability_zone_b
+
+}
